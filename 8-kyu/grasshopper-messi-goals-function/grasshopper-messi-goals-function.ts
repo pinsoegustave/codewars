7f2058f80fcb7b3c7529fab32238bd0a336288fd
Pinsoe Gustave <108759573+pinsoegustave@users.noreply.github.com>
2025-06-23 13:50:41 +0200

    Add solution - CodeHub

---
+++ 8-kyu/grasshopper-messi-goals-function/grasshopper-messi-goals-function.ts
@@ -0,0 +1,5 @@
+export function goals (laLigaGoals:number, copaDelReyGoals:number, championsLeagueGoals:number): number {
+  //show me the goals!
+  
+  return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+}
