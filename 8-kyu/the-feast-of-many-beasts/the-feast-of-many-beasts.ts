1785ead83fbe5357630aea21507f4de9bc2f33d8
Pinsoe Gustave <108759573+pinsoegustave@users.noreply.github.com>
2025-06-25 15:11:22 +0200

    Add solution - CodeHub

---
+++ 8-kyu/the-feast-of-many-beasts/the-feast-of-many-beasts.ts
@@ -0,0 +1,14 @@
+export function feast(beast:string, dish:string):boolean {
+  // Your Code
+  if (beast[0] == dish[0]) {
+    if (beast.charAt(beast.length-1) == dish.charAt(dish.length-1)){
+      return true
+    }
+    else {
+      return false
+    }
+  }
+  else {
+    return false
+  }
+}
